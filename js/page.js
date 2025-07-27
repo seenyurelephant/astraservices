851df7a4a4b6f6c6da51fdecf9ccad26bec4ce8c
seenyurelephant <lucarionitelegendzgamer@gmail.com>
2025-07-27 23:33:40 +0100

    Update page.js

--- js/page.js
+++ js/page.js
@@ -1,1 +1,5 @@
-
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Thank you for your message! Ali will get back to you soon.');
+  this.reset();
+});
